2ee2420fad6e1a2239a04d8017a6f7b743069590
unknown <>
2016-09-20 16:48:31 +0900

    index update

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,41 @@
 (function(window){
 	'use strict';
 
-	window.slider_test.setAutoPlay(800);
+	var play_btn = document.querySelector(".demo button.play");
+	
+	function playSlider() {
+		window.slider_test.setAutoPlay(800);
 
-	var intervals = [];
-
-	setTimeout(function(){
-		window.slider_test.setDistance(1.5);
-		window.slider_test.setPerspectiveOrigin("50% 50%");
-		intervals[0] = setInterval(function(){
+		setTimeout(function(){
 			window.slider_test.setDistance(1.5);
-			window.slider_test.setPerspectiveOrigin("50% 50%");
-		}, 12000);
-	}, 2400);
-	setTimeout(function(){
-		window.slider_test.setAxis("X");
-		window.slider_test.setPerspectiveOrigin("80% 50%");
-		intervals[1] = setInterval(function(){
+		}, 2400);
+		setTimeout(function(){
 			window.slider_test.setAxis("X");
 			window.slider_test.setPerspectiveOrigin("80% 50%");
-		}, 12000);
-	}, 4800);
-	setTimeout(function(){
-		window.slider_test.setDistance(1.09);
-		window.slider_test.setPerspectiveOrigin("50% 50%");
-		window.slider_test.setAxisToRotateZ(30);
-		intervals[2] = setInterval(function(){
+		}, 4800);
+		setTimeout(function(){
 			window.slider_test.setDistance(1.09);
 			window.slider_test.setPerspectiveOrigin("50% 50%");
 			window.slider_test.setAxisToRotateZ(30);
-		}, 12000);
-	}, 7200);
-	setTimeout(function(){
-		window.slider_test.setAxis("Y");
-		window.slider_test.setAxisToRotateZ(0);
-		intervals[3] = setInterval(function(){
+		}, 7200);
+		setTimeout(function(){
 			window.slider_test.setAxis("Y");
 			window.slider_test.setAxisToRotateZ(0);
-		}, 12000);
-	}, 9600);
-	setTimeout(function(){
-		window.slider_test.setDistance(1.2);
-		window.slider_test.setPerspectiveOrigin("20% 80%");
-		intervals[4] = setInterval(function(){
+		}, 9600);
+		setTimeout(function(){
 			window.slider_test.setDistance(1.2);
 			window.slider_test.setPerspectiveOrigin("20% 80%");
 		}, 12000);
-	}, 12000);
+		setTimeout(function(){
+			window.slider_test.setPerspectiveOrigin("50% 50%");
+		}, 14400);
+		setTimeout(function(){
+			window.slider_test.stopAutoPlay();
+		}, 16800);
+	}
 
-	setTimeout(function() {
-		window.slider_test.stopAutoPlay();
-		for(var i=0; i<intervals.length; i++) {
-			clearInterval(intervals[i]);
-		}
-	}, 120000);
+	play_btn.onclick = function() {
+		return playSlider();
+	};
 
 })(this);
