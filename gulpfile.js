075acd4015821debb4a2eba9b1b7f7eb52dd5474
unknown <>
2016-09-22 10:09:34 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,81 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var gulpif = require('gulp-if');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var runSequence = require('run-sequence');
+var del = require('del');
 
+global.isProd = false;
+
+/**
+ * @name dev
+ * @description dev mode gulp task
+ * @type {Function}
+ */
+gulp.task('dev', function () {
+	global.isProd = false;
+
+	runSequence(['clean', 'uglify', 'sass'], 'watch');
+});
+
+/**
+ * @name prod
+ * @description prod mode gulp task
+ * @type {function}
+ */
+gulp.task('prod', ['clean'], function () {
+	global.isProd = true;
+
+	runSequence(['uglify', 'sass']);
+});
+
+/**
+ * @name clean
+ * @description clean dist folder
+ * @type {function}
+ */
+gulp.task('clean', function () {
+	del(['dist']);
+});
+
+/**
+ * @name uglify
+ * @description js file build
+ * @type {function}
+ */
 gulp.task('uglify', function() {
-	return gulp.src('src/js/*.js')
+	gulp.src('src/js/*.js')
+		.pipe(gulpif(!global.isProd, sourcemaps.init()))
+		.pipe(babel({
+			presets : ['es2015']	//es 2015 converting
+		}))
 		.pipe(concat('3d_slider.min.js'))
-		.pipe(uglify())
+		.pipe(gulpif(global.isProd, uglify()))
+		.pipe(gulpif(!global.isProd, sourcemaps.write()))
 		.pipe(gulp.dest('dist'));
 });
 
+/**
+ * @name sass
+ * @description sass converting
+ * @type {function}
+ */
 gulp.task('sass', function() {
-	return gulp.src('src/sass/*.sass')
+	gulp.src('src/sass/*.sass')
 		.pipe(sass())
-		.pipe(gulp.dest('src/css'));
+		.pipe(gulp.dest('dist/css'));
 });
 
+/**
+ * @name watch
+ * @description watch task
+ * @type {function}
+ */
 gulp.task('watch', function() {
 	gulp.watch('src/sass/*.sass', ['sass']);
 	gulp.watch('src/js/*.js', ['uglify']);
 });
 
-gulp.task('default', ['watch']);
-
+//gulp.task('default', ['dev']);
