05b3a11249942a5de4d1ba2a3cd1a2a7f936569a
unknown <>
2016-09-22 09:59:44 +0900

    demo timing set

--- src/js/index.js
+++ src/js/index.js
@@ -4,34 +4,34 @@
 	var play_btn = document.querySelector(".demo button.play");
 	
 	function playSlider() {
-		window.slider_test.setAutoPlay(800);
+		window.slider_test.setAutoPlay(1000);
 
 		setTimeout(function(){
 			window.slider_test.setDistance(1.5);
-		}, 2400);
+		}, 3000);
 		setTimeout(function(){
 			window.slider_test.setAxis("X");
 			window.slider_test.setPerspectiveOrigin("80% 50%");
-		}, 4800);
+		}, 6000);
 		setTimeout(function(){
 			window.slider_test.setDistance(1.09);
 			window.slider_test.setPerspectiveOrigin("50% 50%");
 			window.slider_test.setAxisToRotateZ(30);
-		}, 7200);
+		}, 9000);
 		setTimeout(function(){
 			window.slider_test.setAxis("Y");
 			window.slider_test.setAxisToRotateZ(0);
-		}, 9600);
+		}, 12000);
 		setTimeout(function(){
 			window.slider_test.setDistance(1.2);
 			window.slider_test.setPerspectiveOrigin("20% 80%");
-		}, 12000);
+		}, 15000);
 		setTimeout(function(){
 			window.slider_test.setPerspectiveOrigin("50% 50%");
-		}, 14400);
+		}, 18000);
 		setTimeout(function(){
 			window.slider_test.stopAutoPlay();
-		}, 16800);
+		}, 21000);
 	}
 
 	play_btn.onclick = function() {
